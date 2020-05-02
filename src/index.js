ca9b77a6a8adc7be58d04e05e34da7d8b45c2895
Rodolpho Cavalcanti <rodolphom@gmail.com>
2020-05-02 17:51:44 +0200

    Create list to represent history

--- src/index.js
+++ src/index.js
@@ -74,6 +74,15 @@ class Board extends React.Component {
         const history = this.state.history
         const current = history[history.length-1]
         const winner = calculateWinner(current.squares);
+        const moves = history.map((step,move) => {
+            const desc = move ? 'Go to move: ' + move : 'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+        
         let status;
         
         if (winner) {
@@ -91,7 +100,7 @@ class Board extends React.Component {
           </div>
           <div className="game-info">
             <div>{ status }</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
